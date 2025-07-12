ac44681903ca165e474b9a95206def3c5ddc3f9d
KIKODER <159853177+KIKODER@users.noreply.github.com>
2025-07-11 20:32:16 -0500

    Added calculator background and adjusted styles for buttons.

---
+++ calc.js
@@ -0,0 +1,21 @@
+function add(x, y) {
+    return x + y;
+}
+function sub(x, y) {
+    return x - y;
+}
+function mult(x, y) {
+    return x * y;
+}
+function div(x, y) {
+    return x / y;
+}
+
+let firstNum = undefined;
+let secondNum = undefined;
+let operator = undefined;
+
+console.log(add(3, 4));
+console.log(sub(3, 4));
+console.log(mult(3, 4));
+console.log(div(3, 4));
