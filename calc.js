4bc1b401ffbe418a20b30c4b59bc3752ce601d8f
KIKODER <159853177+KIKODER@users.noreply.github.com>
2025-07-13 21:01:06 -0500

    Fixed proportions of calculator.

--- calc.js
+++ calc.js
@@ -11,11 +11,3 @@ function div(x, y) {
     return x / y;
 }
 
-let firstNum = undefined;
-let secondNum = undefined;
-let operator = undefined;
-
-console.log(add(3, 4));
-console.log(sub(3, 4));
-console.log(mult(3, 4));
-console.log(div(3, 4));
