a17f03851ca0dc900aee6550fb1594c48464dbd6
KIKODER <159853177+KIKODER@users.noreply.github.com>
2025-07-16 14:33:53 -0500

    Completed 'operate' function.

--- calc.js
+++ calc.js
@@ -16,5 +16,20 @@ let op = {};
 let lNum = {};
 
 function operate(firstNum, operator, lastNum) {
-    
+    firstNum = Number(firstNum);
+    lastNum = Number(lastNum);
+
+    switch (operator) {
+        case "+":
+            return add(firstNum, lastNum);
+        case "-":
+            return sub(firstNum, lastNum);
+        case "*":
+            return mult(firstNum, lastNum);
+        case "/":
+            return div(firstNum, lastNum);
+        default:
+            return null;
+    }
 }
+
